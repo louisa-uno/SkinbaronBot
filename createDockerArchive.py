b1351c81e3c63eb81c528a346ae85b32735ca98b
Louis_45 <github@louis45.de>
2021-12-24 01:08:44 +0100

    Delete createDockerArchive.py

--- createDockerArchive.py
+++
@@ -1,6 +0,0 @@
-import tarfile
-
-with tarfile.open('skinbaronDockerArchive.tar', 'w') as archive:
-    archive.add('skinbaron.py')
-    archive.add('Dockerfile')
-    archive.add('requirements.txt')
